54f895cafb66780d2bbce4ac7625ae1c887cac05
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-14 23:01:27 +0100

    Update boot.nix file

--- configuration.d/boot.nix
+++ configuration.d/boot.nix
@@ -5,8 +5,8 @@
         # Kernel version
         kernelPackages = pkgs.linuxPackages_latest;
 
-        # Kernel additional modules
-        # kernelModules = [ ];
+        # Modules that are always loaded by the kernel
+        #kernelModules = [ ];
 
         # Kernel boot parameters
         kernelParams = [
@@ -18,6 +18,16 @@
             options i915 modeset=1 enable_fbc=1 enable_guc=2
             options snd_hda_intel power_save=1
         '';
+
+        # Initial ramdisk
+        initrd = {
+            # The set of kernel modules in the initial ramdisk used during the boot process.
+            # This set must include all modules necessary for mounting the root device
+            availableKernelModules = [ ];
+
+            # Modules that are always loaded by the initrd
+            #kernelModules = [ ];
+        };
 
         # Boot loader (GRUB)
         loader = {
