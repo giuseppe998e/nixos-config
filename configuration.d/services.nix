d29af298729236e5606c41dc1b9970474cd86952
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-14 23:01:57 +0100

    Update services.nix file

--- configuration.d/services.nix
+++ configuration.d/services.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
     services = {
         # PipeWire
@@ -10,6 +12,38 @@
 
             # Use the example session manager (no others are packaged yet so this is enabled by default)
             #media-session.enable = true;
+        };
+
+        # xServer
+        xserver = {
+            enable = true;
+
+            # GDM Display Manager
+            displayManager.gdm = {
+                enable = true;
+                wayland = true;
+            };
+
+            # Gnome Desktop Environment
+            desktopManager.gnome = {
+                enable = true;
+                games.enable = false;
+                excludePackages = [
+                    pkgs.gnome.cheese
+                    pkgs.gnome-photos
+                    pkgs.gnome.gnome-music
+                    pkgs.gnome.gnome-terminal
+                    pkgs.gnome.gnome-characters
+                    pkgs.gnome.totem
+                    pkgs.gnome.tali
+                    pkgs.gnome.iagno
+                    pkgs.gnome.hitori
+                    pkgs.gnome.atomix
+                    pkgs.gnome-tour
+                    pkgs.gnome.geary
+                    pkgs.epiphany
+                ];
+            };
         };
     };
 }
