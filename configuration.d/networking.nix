78e302b40637f858d20e11a20726a399ea243f13
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-14 23:01:48 +0100

    Update networking.nix file

--- configuration.d/networking.nix
+++ configuration.d/networking.nix
@@ -4,11 +4,11 @@
         hostName = "nixos";
 
         # Enable WiFi support (wpa_supplicant)
-        wireless.enable = true;
+        #wireless.enable = true;
 
         # Enable DHCP per-interface
-        useDHCP = false;
-        interfaces.wlp0s20f3.useDHCP = true;
+        #useDHCP = false;
+        #interfaces.wlp0s20f3.useDHCP = true;
 
         # Firewall
         firewall = {
