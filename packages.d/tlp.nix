ab3e0fcde830a958b808e2d5168c4ee931ac649f
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-15 15:42:41 +0100

    Update tlp.nix files

--- packages.d/tlp.nix
+++ packages.d/tlp.nix
@@ -2,7 +2,21 @@
     services.tlp = {
         enable = true;
         settings = {
+            # Battery care
+            START_CHARGE_THRESH_BAT1 = 65;
+            STOP_CHARGE_THRESH_BAT1  = 81;
 
+            # CPU Governor (AC)
+            CPU_SCALING_GOVERNOR_ON_AC = "performance";
+            CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+
+            # CPU Governor (BAT)
+            CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+            CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+            
+            # Bluetooth
+            DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
+            DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth";
         };
     };
 }
