d8f638e7349c97200a0faeb8d43efa028b171943
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-15 15:53:07 +0100

    Fix typo in tlp.nix file

--- packages.d/tlp.nix
+++ packages.d/tlp.nix
@@ -11,8 +11,8 @@
             CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
 
             # CPU Governor (BAT)
-            CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-            CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+            CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
             
             # Bluetooth
             DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
