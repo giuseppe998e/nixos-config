1a1a694ebdaa1d9f88532a63ebbc6f362f61e7e3
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-15 10:59:46 +0100

    Update networking.nix file

--- configuration.d/networking.nix
+++ configuration.d/networking.nix
@@ -10,6 +10,12 @@
         #useDHCP = false;
         #interfaces.wlp0s20f3.useDHCP = true;
 
+        # Nameservers
+        nameservers = [
+            "1.1.1.1"
+            "1.0.0.1"
+        ];
+
         # Firewall
         firewall = {
             enable = true;
