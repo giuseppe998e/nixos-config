fe3c50ed6747fd649e1e02f2856f37eb734266c2
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-15 11:09:22 +0100

    Update hardware.nix file

--- configuration.d/hardware.nix
+++ configuration.d/hardware.nix
@@ -1,8 +1,13 @@
+{ pkgs, ... }:
+
 {
     hardware = {
         cpu.intel.updateMicrocode = true;
         video.hidpi.enable = true;
         pulseaudio.enable = false; # Required for PipeWire (hardware.nix)
-        opengl.enable = true;
+        opengl = {
+            enable = true;
+            extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau intel-ocl ];
+        };
     };
 }
