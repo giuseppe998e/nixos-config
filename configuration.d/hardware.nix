94152884cb70ae96607a9893a87aae36318b2689
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-23 18:56:12 +0100

    Update hardware.nix

--- configuration.d/hardware.nix
+++ configuration.d/hardware.nix
@@ -4,7 +4,7 @@
     hardware = {
         enableRedistributableFirmware = true;
         cpu.intel.updateMicrocode = true;
-        video.hidpi.enable = true;
+        video.hidpi.enable = false;
         opengl = {
             enable = true;
             extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau intel-ocl ];
