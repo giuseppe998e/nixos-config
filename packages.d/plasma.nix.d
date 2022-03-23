41bbe748e92a06d52b97a0e19b1d1a39546786f5
Giuseppe Eletto <peppe.eletto@gmail.com>
2022-03-23 17:21:43 +0100

    Add plasma.nix (disabled by default)

---
+++ packages.d/plasma.nix.d
@@ -0,0 +1,59 @@
+{ pkgs, ... }:
+
+let
+    font-awesome = pkgs.nerdfonts.override {
+        fonts = [ "Hack" ];
+    };
+in
+{
+    services = {
+        xserver = {
+            enable = true;
+
+            # Sddm Display Manager
+            displayManager.sddm = {
+                enable = true;
+                settings = {
+                    Wayland.SessionDir = "${pkgs.plasma5Packages.plasma-workspace}/share/wayland-sessions";
+                };
+            };
+
+            # Plasma Desktop Environment
+            desktopManager.plasma5.enable = true;
+        };
+    };
+
+    environment = {
+        systemPackages = with pkgs; [
+            # Plasma
+            oxygen_gtk
+
+            # Others
+            alacritty
+            firefox # Cause "firefox-devedition-bin(-unwrapped)" not works with "chrome-gnome-shell"
+            celluloid
+        ];
+        
+        shellInit = ''
+            export GTK_PATH=$GTK_PATH:${pkgs.oxygen_gtk}/lib/gtk-2.0
+            export GTK2_RC_FILES=$GTK2_RC_FILES:${pkgs.oxygen_gtk}/share/themes/oxygen-gtk/gtk-2.0/gtkrc
+        '';
+        
+        sessionVariables = {
+            TERMINAL = "alacritty";
+            EDITOR = "gedit";
+        };
+    };
+
+    # Install recommended fonts
+    fonts.fonts = with pkgs; [
+        font-awesome
+        noto-fonts-emoji
+    ];
+
+    # Firefox configurations
+    nixpkgs.config.firefox = {
+        enableGnomeExtensions = true;
+        pipewireSupport = true;
+    };
+}
